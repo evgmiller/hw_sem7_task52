67bc104182732927af38f4b9d78320b312a49065
Anna <nusiksuper@gmail.com>
2022-12-21 12:41:42 +0300

    решение 52 задачи

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}  ");
+
+        }
+        Console.WriteLine();
+    }
+}
+
+void MiddleMultiSum(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double MidSum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            MidSum = array[i, j] + MidSum;
+        }
+        Console.Write($"{MidSum / array.GetLength(0)}  ");
+    }
+}
+
+int[,] array = GetArray(4, 4, 0, 10);
+PrintArray(array);
+Console.WriteLine();
+MiddleMultiSum(array);
